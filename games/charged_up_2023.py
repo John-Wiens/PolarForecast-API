2a16efdd4b2c27bffb594710be0e36b7ecee8deb
john-wiens <jwiens3141@gmail.com>
2023-03-20 18:35:56 -0600

    Added Cache to Improve Search Performance

--- games/charged_up_2023.py
+++ games/charged_up_2023.py
@@ -12,6 +12,9 @@ class ChargedUp2023(FRCGame):
 
         self.stats = [
             CustomStat('rank', self.assign_ranks, report_stat=True),
+            SumStat('opr',[], display_name='OPR', report_stat = True),
+            SumStat('expectedRanking',[], display_name='Expected Ranking', report_stat = True),
+            SumStat('schedule',[], display_name='Schedule', report_stat = True),
 
             # Declare all Source Stats
             Stat('_autoCommunity_T_0'),
@@ -216,7 +219,7 @@ class ChargedUp2023(FRCGame):
             ], display_name="OPR", report_stat = True),
 
             SumStat('simulatedRanking',[]),
-            SumStat('expectedRanking',[], display_name='Expected Ranking', report_stat = True),
+            # SumStat('expectedRanking',[], display_name='Expected Ranking', report_stat = True),
             CustomStat('schedule', self.calc_schedule, display_name='Schedule', report_stat = True)
             
 
