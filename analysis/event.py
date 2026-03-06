cd008efa08616fe604188b9c54011c89ef4b2866
John-Wiens <jwiens3141@gmail.com>
2026-03-05 22:47:26 -0700

    Removing dead code

--- analysis/event.py
+++ analysis/event.py
@@ -47,35 +47,9 @@ class Event():
         print(f"    Updated Match Predictions.")
         
 
-    def ml(self):
-        with open('ml.csv', 'w') as ml:
-            count = 0
-            for match in self.tba_matches:
-                count +=1
-                for team_key in match.get('alliances',{}).get('blue',{}).get('team_keys',[]):
-                    auto = self.teams.get(team_key,{}).get('auto',0)
-                    cargo = self.teams.get(team_key,{}).get('cargo',0)
-                    endgame = self.teams.get(team_key,{}).get('endgame',0)
-                    ml.write(f'{auto},{cargo},{endgame},')
-                    
-
-                for team_key in match.get('alliances',{}).get('red',{}).get('team_keys',[]):
-                    auto = self.teams.get(team_key,{}).get('auto',0)
-                    cargo = self.teams.get(team_key,{}).get('cargo',0)
-                    endgame = self.teams.get(team_key,{}).get('endgame',0)
-                    ml.write(f'{auto},{cargo},{endgame},')
-
-
-
-
-                blue_score = match['score_breakdown']['blue']['totalPoints']
-                red_score = match['score_breakdown']['red']['totalPoints']
-                ml.write(f'{blue_score},{red_score}\n')
-
     # Update Team Performances Based on Latest available TBA Data
     def update_team_info(self):
         matches = self.get_sanitized_matches(self.tba_matches)
-        print(matches[0])
         played_matches = self.get_played_matches(matches)
         teams = self.create_team_lookup(self.tba_teams, self.tba_rankings, matches)
         if len(teams) == 0:
--- games/rebuilt_2026.py
+++ games/rebuilt_2026.py
@@ -106,8 +106,9 @@ class Rebuilt2026(FRCGame):
 
     # Assigns Event Rankings to all the Teams at the event
     def assign_ranks(self, played_matches:list, teams:list, stat:dict, rankings:dict)-> dict:
-        for rank in rankings.get('rankings', []):
-            teams[rank['team_key']]['rank'] = rank['rank']
+        if rankings is not None:
+            for rank in rankings.get('rankings', []):
+                teams[rank['team_key']]['rank'] = rank['rank']
         return teams
 
     def validate_match(self, match:dict) -> bool:
